00e4eccf9473073eec55fd7aaf78eb63d1051be6
Zach Weaver <49159969+Kiyoshika@users.noreply.github.com>
2023-06-11 18:18:17 -0400

    fix weird formatting in json.h

--- include/json.h
+++ include/json.h
@@ -16,7 +16,7 @@ enum json_type_e
   JSON_NOTYPE,
   JSON_INT32,
   JSON_DECIMAL,
-	JSON_STRING,
+  JSON_STRING,
   JSON_OBJECT,
   JSON_ARRAY,
   JSON_BOOL,
@@ -25,7 +25,7 @@ enum json_type_e
 
 struct json_item_t
 {
-	enum json_type_e type;
+  enum json_type_e type;
   union value
   {
     int32_t int32;
@@ -36,14 +36,14 @@ struct json_item_t
     bool boolean;
     bool is_null;
   } value;
-	char key[JSON_MAX_KEY_LEN];
+  char key[JSON_MAX_KEY_LEN];
   size_t key_len;
 };
 
 struct json_t
 {
-	struct json_item_t* items;
-	size_t n_items;
+  struct json_item_t* items;
+  size_t n_items;
   size_t capacity;
 };
 
@@ -60,50 +60,50 @@ json_free(
 bool
 json_add_item(
   struct json_t* const json,
-	const enum json_type_e type,
+  const enum json_type_e type,
   const char* const key,
   void* value);
 
 bool
 json_add_int32(
-	struct json_t* const json,
-	const char* const key,
-	int32_t value);
+  struct json_t* const json,
+  const char* const key,
+  int32_t value);
 
 bool
 json_add_decimal(
-	struct json_t* const json,
-	const char* const key,
-	double value);
+  struct json_t* const json,
+  const char* const key,
+  double value);
 
 bool
 json_add_bool(
-	struct json_t* const json,
-	const char* const key,
-	bool value);
+  struct json_t* const json,
+  const char* const key,
+  bool value);
 
 bool
 json_add_string(
-	struct json_t* const json,
-	const char* const key,
-	char* const value);
+  struct json_t* const json,
+  const char* const key,
+  char* const value);
 
 bool
 json_add_object(
-	struct json_t* const json,
-	const char* const key,
-	struct json_t* const value);
+  struct json_t* const json,
+  const char* const key,
+  struct json_t* const value);
 
 bool
 json_add_array(
-	struct json_t* const json,
-	const char* const key,
-	struct json_array_t* value);
+  struct json_t* const json,
+  const char* const key,
+  struct json_array_t* value);
 
 bool
 json_add_null(
-	struct json_t* const json,
-	const char* const key);
+  struct json_t* const json,
+  const char* const key);
 
 struct json_t*
 json_parse_from_string(
